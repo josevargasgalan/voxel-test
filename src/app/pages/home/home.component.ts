2aa7f2170347c027157e00a3cc5de5fc1e91e1c5
josevargasgalan <josevargasgalan@gmail.com>
2023-02-07 17:45:54 +0100

    Add Card component into Home Component
    In order to add Card component into Home component:
    - Add GetCardInfo method to send information needed to the Card
    - Set components workflow
    - Add component template
    - Add styles and media queries

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CardInfo } from 'src/app/components/card/models';
+import { TVShowDetail } from '../../models';
+import { ApiService } from '../../services';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  tvShows: TVShowDetail[] = [];
 
+  constructor(private apiService: ApiService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.apiService.getTVShows(6).subscribe(tvShows => {
+      this.tvShows = tvShows;
+    })
+  }
+
+  getCardInfo(tvShow: TVShowDetail): CardInfo {
+    return {
+      title: tvShow.name,
+      image: tvShow.image.medium,
+      content: `Average: ${tvShow.rating.average || 0}`,
+      clickAction: () => this.router.navigate([`/detail`], {queryParams: { id: tvShow.id }})
+    }
+  }
 }
