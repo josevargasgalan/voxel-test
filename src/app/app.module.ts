1427f1c459fd05ec35e2154f300fc5545d290eaa
josevargasgalan <josevargasgalan@gmail.com>
2023-02-07 16:09:16 +0100

    Add pages
    In order to add different pages:
    - Add empty home page
    - Add empty detail page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
