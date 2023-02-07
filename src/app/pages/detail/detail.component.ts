f197d2b8df87c7f285c09f5210ac643462bc26e1
josevargasgalan <josevargasgalan@gmail.com>
2023-02-07 19:37:32 +0100

    Add detail page
    In order to add detail page:
    - Add back button to navigate to home page
    - Add detail image
    - Add chips list component to show genres
    - Add title and summary

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/services';
+import { switchMap } from 'rxjs/operators';
+import { Image, Rating, TVShowDetail } from 'src/app/models';
 
 @Component({
   selector: 'app-detail',
@@ -7,4 +11,30 @@ import { Component } from '@angular/core';
 })
 export class DetailComponent {
 
+  tvShow: TVShowDetail = {
+      name: '',
+      image: {} as Image,
+      rating: {} as Rating,
+      genres: []
+  }
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.getTvShow();
+  }
+
+  public backToHomePage() {
+    this.router.navigate(['/']);
+  }
+
+
+  private getTvShow() {
+    this.activatedRoute.queryParams.pipe(
+      switchMap(params => this.apiService.getTVShowById(params['id']),
+    ))
+    .subscribe(tvShow => {
+      this.tvShow = tvShow;
+    })
+  }
 }
--- src/app/pages/detail/detail.module.ts
+++ src/app/pages/detail/detail.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { DetailComponent } from './detail.component';
 import { Route, RouterModule } from '@angular/router';
+import { MatChipsModule } from '@angular/material/chips'
 import { CommonModule } from '@angular/common';
 import { AverageComponent } from 'src/app/components/average/average.component';
 
@@ -17,7 +19,9 @@ const routes: Route[] = [{
   ],
   imports: [
     CommonModule,
+    MatButtonModule,
     MatIconModule,
+    MatChipsModule,
     RouterModule.forChild(routes)
   ],
   providers: []
